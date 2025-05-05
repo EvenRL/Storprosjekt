7c031996fe202df97f6c561bf6029a4fe26eec46
EvenRL <52103879+EvenRL@users.noreply.github.com>
2025-05-06 01:39:09 +0200

    3D Cube detection

--- cube_detector/cube_detector/color_detection.py
+++ cube_detector/cube_detector/color_detection.py
@@ -8,11 +8,11 @@ import numpy as np
 
 class ColorDetectionNode(Node):
     """
-    Node for detecting colors blobs
+    Node for detecting colored cubes
     """
     color = (255,255,255)
 
-    # Colors to check search for in hsv format
+    # Colors to search for. hsv format
     colors = {'blue': [np.array([90, 50, 70]), np.array([128, 255, 255])],
         'red': [np.array([159, 50, 70]), np.array([180, 255, 255])],
         'yellow': [np.array([25, 50, 70]), np.array([35, 255, 255])],
@@ -29,6 +29,58 @@ class ColorDetectionNode(Node):
                 cx = int(M['m10'] / M['m00']) # calculate X position
                 cy = int(M['m01'] / M['m00']) # calculate Y position
                 return c, cx, cy
+
+    def findColorContours(self, image):
+        hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV) # create image copy in hsv format
+
+        for name, clr in self.colors.items():
+            if self.find_color(hsv, clr):
+                c, cx, cy = self.find_color(hsv, clr)
+                cv.drawContours(image, [c], -1, self.color, 3) #draw contours
+                cv.circle(image, (cx, cy), 7, self.color, -1)  # draw circle
+                cv.putText(image, name, (cx,cy), 
+                            cv.FONT_HERSHEY_SIMPLEX, 1, self.color, 1) # put text
+        return image
+
+    def findCubeContours(self, image):
+        camera_matrix = np.array([[623.60721, 0.0, 297.00864],
+                         [0.0, 620.86526, 244.34018],
+                         [0.0, 0.0, 1.0]], dtype=np.float32)
+        object_points = np.array([[0,0,0],[0.05,0,0],[0.05,0.05,0],[0,0.05,0]], dtype=np.float32)
+        dist_coeffs = np.array([-0.019917, -0.022530, 0.002542, -0.009390, 0.000000])
+        cube_points = np.array([[0,0,0],[0.05,0,0],[0.05,0.05,0],[0,0.05,0],
+                                [0,0,-0.05],[0.05,0,-0.05],[0.05,0.05,-0.05],[0,0.05,-0.05]])
+
+        hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV) # create image copy in hsv format
+        for name, clr in self.colors.items():
+            mask = cv.inRange(hsv, clr[0], clr[1])
+            cnts, _ = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+            for cnt in cnts:
+                area = cv.contourArea(cnt) # find how big countour is
+                if 2500 < area < 10000:       # only if countour is big enough
+                    epsilon = 0.05 * cv.arcLength(cnt, True)
+                    approx = cv.approxPolyDP(cnt, epsilon, True)
+
+                    if len(approx) == 4:
+                        corners = approx.reshape(-1,2)
+                        for point in corners:
+                            cv.circle(image, tuple(point), radius=5, color=(0,255,0), thickness=-1)
+                        image_points = np.array(corners, dtype=np.float32)
+
+                        success, rvec, tvec = cv.solvePnP(object_points, image_points, camera_matrix, dist_coeffs)
+                        imgpts, _ = cv.projectPoints(cube_points, rvec, tvec, camera_matrix, dist_coeffs)
+                        imgpts = np.int32(imgpts).reshape(-1,2)
+
+                        cv.drawContours(image,[imgpts[:4]], -1, (255,0,0), 2)
+                        for i in range(4):
+                            cv.line(image, tuple(imgpts[i]), tuple(imgpts[i+4]), (0,255,0), 2)
+                        
+                        cv.drawContours(image, [imgpts[4:]], -1, (0,0,255), 2)
+
+        return image
+
+
+        
 
     def __init__(self):
         super().__init__('color_detection')
@@ -57,23 +109,15 @@ class ColorDetectionNode(Node):
             self.get_logger().error('Failed to convert image: %s' % str(e))
             return
 
-        hsv = cv.cvtColor(cv_image, cv.COLOR_BGR2HSV)
-
-        for name, clr in self.colors.items():
-            if self.find_color(hsv, clr):
-                c, cx, cy = self.find_color(hsv, clr)
-                cv.drawContours(cv_image, [c], -1, self.color, 3) #draw contours
-                cv.circle(cv_image, (cx, cy), 7, self.color, -1)  # draw circle
-                cv.putText(cv_image, name, (cx,cy), 
-                            cv.FONT_HERSHEY_SIMPLEX, 1, self.color, 1) # put text
+        new_image = self.findCubeContours(cv_image)
 
         try:
-            blur_msg = self.bridge.cv2_to_imgmsg(cv_image, "bgr8")
+            cube_msg = self.bridge.cv2_to_imgmsg(new_image, "bgr8")
         except Exception as e:
             self.get_logger().error('Failed to convert image: %s' % str(e))
             return
   
-        self.publisher.publish(blur_msg)
+        self.publisher.publish(cube_msg)
 
 # Initialize the node
 def main(args=None):
