01d3b96f207d47b7f1c2d30e954578948bd006e9
siverbl <142888477+siverbl@users.noreply.github.com>
2025-05-11 23:01:37 +0200

    Update setup.py
    transformation

--- cube_detector/setup.py
+++ cube_detector/setup.py
@@ -20,7 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'color_detection = cube_detector.color_detection:main'
+            'color_detection = cube_detector.color_detection:main',
+            'transformation = cube_detector.transformation:main'
         ],
     },
 )
