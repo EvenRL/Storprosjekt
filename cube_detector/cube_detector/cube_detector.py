bd0beffd7a9147ec150ce7c2acc15b8acfe0f578
EvenRL <even.r.landro@gmail.com>
2025-05-25 21:11:50 +0200

    Update blur

--- cube_detector/cube_detector/cube_detector.py
+++ cube_detector/cube_detector/cube_detector.py
@@ -30,6 +30,7 @@ class CubeDetectorNode(Node):
         self.declare_parameter('maxArea', 30000) # Maximum contour area
         self.declare_parameter('alpha', 0.05) # Epsilon factor for contour approximation, % of contour perimeter
         self.declare_parameter('enableBlur', False) # Enables preprocessing image with gaussian blur
+        self.declare_parameter('blurKernelSize', 5) # Kernel size for gaussian blur
         self.declare_parameter('debug', False) # Enable debug mode
         self.declare_parameter('debug_draw_contours', False) # Draw select contours in debug mode
         self.declare_parameter('debug_draw_all_contours', True) # Draw all contours in debug mode
@@ -41,6 +42,7 @@ class CubeDetectorNode(Node):
         self.maxArea = self.get_parameter('maxArea').value
         self.alpha = self.get_parameter('alpha').value
         self.enableBlur = self.get_parameter('enableBlur').value
+        self.blurKernelSize = self.get_parameter('blurKernelSize').value
         self.debug = self.get_parameter('debug').value
         self.debug_draw_contours = self.get_parameter('debug_draw_contours').value
         self.debug_draw_all_contours = self.get_parameter('debug_draw_all_contours').value
@@ -132,7 +134,7 @@ class CubeDetectorNode(Node):
             return
 
         if self.enableBlur:
-            cv_image = cv.blur(cv_image,(5,5))
+            cv_image = cv.blur(cv_image,(self.blurKernelSize,self.blurKernelSize))
 
         if self.debug:
             # Debug mode
